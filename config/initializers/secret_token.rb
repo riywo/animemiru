ebf16019ac832157410f6d536450ff95e8a1e14f
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-02-16 13:21:40 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Animemiru::Application.config.secret_token = '873c7bdbf8bb2fee94b42d677ac323fa20ca027bc6d7b73cc2bdca46396e47a9b81448b25caeb00469ed2db67451c3725d933d6647e7ebf2b04fe91a0e8e8496'

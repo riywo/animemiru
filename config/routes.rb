6f4a754fe885ea5a80d8dc3cd8444dd1859c7ff8
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-02-16 20:54:48 -0800

    initializer, model, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Animemiru::Application.routes.draw do
   get "home/index"
 
+  match "/auth/:provider/callback" => "sessions#create"
+  match "/signout" => "sessions#destroy", :as => :signout
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

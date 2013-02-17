1bbb12be860c3fb35395c18710d88e648ee39f1c
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-02-16 20:58:22 -0800

    link

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :provider, :uid
 
-  def self.from_omniauth
+  def self.from_omniauth(auth)
     find_by_provider_and_uid(auth["provider"], auth["uid"]) || create_with_omniauth(auth)
   end
 
